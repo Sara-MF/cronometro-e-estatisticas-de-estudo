45c510b17baf95772cba2dd7ab9254a7b3ea55bc
sara-mf <sara.melo.dev@gmail.com>
2025-03-26 20:50:11 -0300

    feat: ajuste nos selects, para escolher o tema somente apos a escolha da disciplina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
+'use client'
+
 import Select from '@/components/Select';
 import Timer from '@/components/Timer';
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [subject, setSubject] = useState("");
+
+  const subjectTopics: Record<string, string[]> = {
+    Português:  ["Gramática", "Literatura", "Redação"],
+    História:   ["Idade Média", "Brasil Colônia", "Revolução Francesa"],
+    Ciências:   ["Biologia", "Física", "Química"],
+    Geografia:  ["Climatologia", "Geopolítica", "Cartografia"],
+    Matemática: ["Álgebra", "Geometria", "Probabilidade"],
+  }
+
   return (
     <div className="lg:w-1/3 flex flex-col gap-12 p-6 rounded-lg shadow-lg bg-base-300">
 
       <div className="w-full flex flex-row justify-between gap-4">
-        <Select type="disciplina" />
+        <Select type="disciplina" onChange={(value) => setSubject(value)}/>
 
-        <Select type="tema" />
+        <Select type="tema" options={subjectTopics[subject] || ["Selecione um tema"]} disabled={!subject} />
       </div>
 
       <Timer/>
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,22 +1,30 @@
 type SelectProps = {
-  type: string;
+  type: "disciplina" | "tema";
+  options?: string[];
+  disabled?: boolean;
+  onChange?: (value: string) => void;
 };
-export default function Select({ type }: SelectProps) {
+
+export default function Select({ type, options = [], disabled = false, onChange }: SelectProps) {
 
   const title = type === "disciplina" ? "Selecione uma disciplina" : "Selecione um tema";
 
+  const subjects = ["Português", "História", "Ciências", "Geografia", "Matemática"];
+
   return (
 
-    <select defaultValue={title} className="select">
-      <option disabled={true}>{title}</option>
-      <option>Português</option>
-      <option>História</option>
-      <option>Ciências</option>
-      <option>Geografia</option>
-      <option>Matemática</option>
+    <select defaultValue={title} className="select" disabled={disabled} onChange={(e) => onChange && onChange(e.target.value)}>
+      {type === "disciplina" ? (
+          <option value={title} disabled>
+          {title}
+        </option>
+      ) : null}
+
+      {(type === "disciplina" ? subjects : options || []).map((option) => (
+        <option key={option}>{option}</option>
+      ))}
     </select>
   )
 }
-
 
 
