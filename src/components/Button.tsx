a2e942c13ec38f0e576fbe334cdd1d2f9d1c61db
sara-mf <sara.melo.dev@gmail.com>
2025-03-26 19:31:16 -0300

    feat: adicionando funcionalidade para os botoes do cronometro e criando a estrutura do modal de confirmacao ao encerrar o cronometro

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,15 @@
 type ButtonProps = {
   name: string;
   color: string;
+  onClick: () => void;
 };
 
-export default function Button({ name, color }: ButtonProps) {
+export default function Button({ name, color, onClick }: ButtonProps) {
 
   const styles = "btn btn-sm lg:btn-lg btn-soft " + color;
 
   return (
 
-    <button type="button" className={styles}>{name}</button>
+    <button type="button" className={styles} onClick={onClick}>{name}</button>
   )
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,19 +1,68 @@
 'use client'
 
 import Button from '@/components/Button';
+import { useRef, useState, useEffect } from 'react';
 
 export default function Timer() {
 
+  // Modal
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  const showModal = () => {
+    modalRef.current?.showModal();
+  };
+
+  // Contador
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let timer : NodeJS.Timeout;
+    if (running) {
+      timer  = setInterval(() => {
+        setTime((previousTime) => previousTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [running]);
+
+  const formatTimer = (seconds: number) => {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    return `${String(min).padStart(2, "0")}:${String(sec).padStart(2, "0")}`;
+  };
+
   return (
     <div className="w-full flex flex-col justify-center items-center gap-12">
-      <h1 className="text-6xl">00:00</h1>
+      <h1 id="timer" className="text-6xl">{formatTimer(time)}</h1>
 
       <div className="flex justify-evenly w-full">
-        <Button name="Iniciar" color="btn-success"></Button>
-        <Button name="Pausar" color="btn-accent"></Button>
-        <Button name="Resetar" color="btn-error"></Button>
-        <Button name="Salvar" color="btn-success" ></Button>
+
+        {!running ? (
+          <Button name="Iniciar" color="btn-success" onClick={() => setRunning(!running)} />
+        ) : (
+          <Button name="Pausar" color="btn-accent" onClick={() => setRunning(!running)} />
+        )}
+
+        <Button name="Resetar" color="btn-error" onClick={() => { setTime(0); setRunning(false); }}></Button>
+        <Button name="Salvar tempo" color="btn-success" onClick={showModal}></Button>
       </div>
+
+
+        <dialog ref={modalRef} className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Encerrar contador</h3>
+            <p className="py-4">Tem certeza que deseja encerrar o contador?</p>
+            <div className="modal-action flex flex-row justify-end">
+              {/* <form method="dialog"> */}
+                <button className="btn btn-success" onClick={() => modalRef.current?.close()}>Encerrar</button>
+                <button className="btn btn-warning" onClick={() => modalRef.current?.close()}>Voltar</button>
+              {/* </form> */}
+            </div>
+          </div>
+        </dialog>
 
     </div>
 
